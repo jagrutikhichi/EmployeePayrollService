feb3aa92d4a69045ffa00e2e32efb301e80f1619
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-20 23:53:40 +0530

    Added: Employee payroll to console

---
+++ src/test/java/EmployeePayrollService/EmployeePayrollData.java
@@ -0,0 +1,33 @@
+package EmployeePayrollService;
+
+public class EmployeePayrollData {
+	private String name;
+	private int id;
+	private double salary;
+	public EmployeePayrollData(String name, int id, double salary) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.salary = salary;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+}
