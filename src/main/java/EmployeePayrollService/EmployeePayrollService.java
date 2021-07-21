5546fa2a2c9a902cd793adbd5353cf846081c297
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-21 18:15:00 +0530

    Added: program to store Employee payroll into file

--- src/main/java/EmployeePayrollService/EmployeePayrollService.java
+++ src/main/java/EmployeePayrollService/EmployeePayrollService.java
@@ -5,6 +5,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+	}
+
 	private List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
 
 	public EmployeePayrollService() {
@@ -29,6 +34,22 @@ public class EmployeePayrollService {
 
 	private void writeEmployeePayrollData() {
 		System.out.println("Writing employee payroll to console\n" + employeePayrollDataList);
+	}
+
+	public void writeEmployeeData(IOService ioService) {
+		if (ioService.equals(IOService.CONSOLE_IO))
+			System.out.println("Writing Employee Payroll Roster in Console\n" + employeePayrollDataList);
+		else if (ioService.equals(IOService.FILE_IO)) {
+			EmployeePayrollFileIOService.writeData(employeePayrollDataList);
+		}
+	}
+
+	public long countEntries(IOService ioService) {
+		if (ioService.equals(IOService.CONSOLE_IO))
+			return employeePayrollDataList.size();
+		else if (ioService.equals(IOService.FILE_IO))
+			return EmployeePayrollFileIOService.countEntries();
+		return 0;
 	}
 
 	public static void main(String[] args) {
--- src/test/java/EmployeePayrollService/NIOFileAPITest.java
+++ src/test/java/EmployeePayrollService/NIOFileAPITest.java
@@ -11,35 +11,60 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class NIOFileAPITest {
-	private static String HOME = System.getProperty("user.home");
-	private static String PLAY_WITH_NIO = "TempPlayGround";
+	public static final String HOME = System.getProperty("user.home");
+    public static final String PLAY_WITH_NIO = "TempPlayground";
 
-	@Test
-	public void givenPathWhenCheckedThenConfirm() throws IOException {
-		Path homePath = Paths.get(HOME);
-		Assert.assertTrue(Files.notExists(homePath));
+    @Test
+    public void givenPath_whenChecked_thenConfirm() throws IOException {
+        //check file Exists
+        Path homePath = Paths.get(HOME);
+        Assert.assertTrue(Files.exists(homePath));
 
-		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
-		if (Files.exists(playPath))
-			FileUtils.deleteFiles(playPath.toFile());
-		Assert.assertTrue(Files.exists(playPath));
+        //Delete file and check file not exist
+        Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        if (Files.exists(playPath))
+            FileUtils.deleteFiles(playPath.toFile());
+        Assert.assertTrue(Files.notExists(playPath));
 
-		Files.createDirectory(playPath);
-		Assert.assertTrue(Files.exists(playPath));
+        //Create Directory
+        Files.createDirectory(playPath);
+        Assert.assertTrue(Files.exists(playPath));
 
-		IntStream.range(1, 10).forEach(cntr -> {
-			Path tempFile = Paths.get(playPath + "/temp" + cntr);
-			Assert.assertTrue(Files.notExists(tempFile));
-			try {
-				Files.createFile(tempFile);
-			} catch (IOException e) {
-			}
-			Assert.assertTrue(Files.exists(tempFile));
-		});
+        //create file
+        IntStream.range(1, 10).forEach(counter -> {
+            Path tempFile = Paths.get(playPath + "/temp" + counter);
+            Assert.assertTrue(Files.notExists(tempFile));
 
-		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
-		Files.newDirectoryStream(playPath).forEach(System.out::println);
-		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
-				.forEach(System.out::println);
-	}
+            File myFile = new File(tempFile.toString());
+            try {
+                Files.createFile(tempFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Assert.assertTrue(Files.exists(tempFile));
+        });
+
+        Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+        Files.newDirectoryStream(playPath).forEach(System.out::println);
+        Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp")).forEach(System.out::println);
+
+    }
+    @Test
+    void givenDirectory_WhenWatched_listsAllTheActivites() throws IOException {
+        Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new FileWatchService(dir).processEvents();
+    }
+    @Test
+    public void numberOfEmployeeEntryTest() {
+        EmployeePayrollData[] empArray = {
+                new EmployeePayrollData("Jeff Bezos", 1, 100000.0),
+                new EmployeePayrollData("Bill Gates", 2, 200000.0),
+                new EmployeePayrollData("Mark Zuckerberg", 3, 300000.0)
+        };
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        employeePayrollService.writeEmployeeData(EmployeePayrollService.IOService.FILE_IO);
+        long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        Assert.assertEquals(3, entries);
+    }
 }
